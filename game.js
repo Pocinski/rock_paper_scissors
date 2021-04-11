d5003e053702f6324e0c5dabec0f11cf6099c606
Pocinski <sahinerugur13@gmail.com>
2021-04-11 22:08:44 +0300

    Add game.js and blank.html this time

---
+++ game.js
@@ -0,0 +1,22 @@
+function computerPlay() {
+
+    let things = ["Rock", "Paper", "Scissors"];
+    return things[Math.floor(Math.random() * things.length)];
+}
+
+
+const playerSelection =  ;
+
+const computerSelection = computerPlay();
+console.log((playRound));
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "Tie!";
+    }
+
+    else if ()
+}
+
