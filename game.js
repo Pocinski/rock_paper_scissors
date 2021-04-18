abec79a6e669b5b07c1d764501f2172130638432
Pocinski <sahinerugur13@gmail.com>
2021-04-18 21:08:58 +0300

    Add game function and merge with other functions

--- game.js
+++ game.js
@@ -1,42 +1,49 @@
-function computerPlay() {
+function game() {
 
-    let things = ["Rock", "Paper", "Scissors"];
-    return things[Math.floor(Math.random() * things.length)];
-}
+    function computerPlay() {
 
-
-const playerSelection =  ;
-
-const computerSelection = computerPlay();
-console.log((playRound));
-
-
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return "Tie!";
+        let things = ["Rock", "Paper", "Scissors"];
+        return things[Math.floor(Math.random() * things.length)];
     }
 
-    else {
-        switch (playerSelection.toLowerCase()) {
-            case "rock":
-                if (computerSelection.toLowerCase() == "scissors")
-                    return "You win! You rock hard!!";
-                else
-                    return "You lose! Paper got you";
 
-            case "paper":
-                if (computerSelection.toLowerCase() == "rock")
-                    return "You win! You wrapped!!";
-                else
-                    return "You lose! scissors cut you";
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+            return "Tie!";
+        }
 
-            case "scissors":
-                if (computerSelection.toLowerCase() == "paper")
-                    return "You win! You cut!!";
-                else
-                    return "You lose! Rock beated you"; s
+        else {
+            switch (playerSelection.toLowerCase()) {
+                case "rock":
+                    if (computerSelection.toLowerCase() == "scissors")
+                        return "You win! You rock hard!!";
+                    else
+                        return "You lose! Paper got you";
+
+                case "paper":
+                    if (computerSelection.toLowerCase() == "rock")
+                        return "You win! You wrapped!!";
+                    else
+                        return "You lose! scissors cut you";
+
+                case "scissors":
+                    if (computerSelection.toLowerCase() == "paper")
+                        return "You win! You cut!!";
+                    else
+                        return "You lose! Rock beated you";
+            }
         }
     }
+
+    for (let i = 0; i > 5; i++) {
+
+        const playerSelection = prompt("Enter a number");
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+
+    }
+
+
+
 }
 
